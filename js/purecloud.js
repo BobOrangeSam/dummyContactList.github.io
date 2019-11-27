7196bee47f88584d253c68c0606a7263be3044c7
BobOrangeSam <58166910+BobOrangeSam@users.noreply.github.com>
2019-11-27 12:04:08 +0100

    Update purecloud.js

--- js/purecloud.js
+++ js/purecloud.js
@@ -166,6 +166,135 @@ function getContactListByName(listname) {
 		var apiInstance = new platformClient.OutboundApi();
 		
 		var opts = { 
+/**
+ *
+ * @returns {object} current user details
+ */
+function getCurrentUser() {
+  return new Promise(function(resolve, reject) {
+    try {
+		var apiInstance = new platformClient.UsersApi();
+		let opts = { 
+			'expand': ["routingStatus, presence"] // routingStatus, presence, conversationSummary, outOfOffice, geolocation, station, authorization, authorization.unusedRoles, profileSkills, certifications, locations, groups, skills, languages, languagePreference, employerInfo, biography, date, geolocationsettings, organization, presencedefinitions, locationdefinitions, orgauthorization, orgproducts, favorites, superiors, directreports, adjacents, routingskills, routinglanguages, fieldconfigs, token, trustors
+		};
+
+        apiInstance
+			.getUsersMe(opts)
+			.then(function(data) {
+				//console.log("getUsersMe start");
+				//console.log(data);
+				//console.log("getUsersMe done");
+				resolve(data);
+			})
+			.catch(function(err) {
+				console.log("There was a failure calling getUsersMe");
+				console.error(err);
+				reject(err);
+			});
+    } catch (err) {
+      console.log(err);
+      reject(err);
+    }
+  });
+}
+
+/**
+ *
+ * @param {string} listname Name of the new contact list
+ *
+ * @returns {object} contact list information
+ */
+function createContactList(listname) {
+  return new Promise(function(resolve, reject) {
+    try {
+		var apiInstance = new platformClient.OutboundApi();
+		var body = {
+		   "name": listname,
+		   "columnNames": ["uniqueid","firstName","lastName","Telephone"],
+		   "phoneColumns": [
+			  {
+				 "columnName": "Telephone",
+				 "type": "cell",
+				 "callableTimeColumn": ""
+			  }
+		   ]
+		};
+
+        apiInstance
+			.postOutboundContactlists(body)
+			.then(function(data) {
+				console.log("createContactList start");
+				console.log(data);
+				console.log("createContactList done");
+				resolve(data);
+			})
+			.catch(function(err) {
+				console.log("There was a failure calling createContactList");
+				console.error(err);
+				reject(err);
+			});
+    } catch (err) {
+      console.log(err);
+      reject(err);
+    }
+  });
+}
+
+/**
+ *
+ * @param {string} uniqueId Unique ID for this item
+ * @param {string} contactListId ID of the contact list
+ *
+ * @returns {object} contact list information
+ */
+function addContactInList(uniqueId,contactListId,firstName,lastName,telephone) {
+  return new Promise(function(resolve, reject) {
+    try {
+		var apiInstance = new platformClient.OutboundApi();
+		
+		var body = [{ 
+		  "id": "", 
+		  "contactListId": contactListId, 
+		  "data": {"uniqueid":uniqueId,"firstName":firstName,"lastName":lastName,"Telephone":telephone}
+		}];
+		var opts = {
+			'priority': false, // True means the contact(s) will be dialed next; false means the contact will go to the end of the contact queue.
+			'clearSystemData': true, // Boolean | Clear system data. True means the system columns (attempts, callable status, etc) stored on the contact will be cleared if the contact already exists; false means they won't.
+			'doNotQueue': true // Boolean | Do not queue. True means that updated contacts will not have their positions in the queue altered, so contacts that have already been dialed will not be redialed. For new contacts they will not be called until a campaign recycle; False means that updated contacts will be re-queued, according to the 'priority' parameter.
+		};
+
+        apiInstance
+			.postOutboundContactlistContacts(contactListId, body, opts)
+			.then(function(data) {
+				console.log("addContactInList start");
+				console.log(data);
+				console.log("addContactInList done");
+				resolve(data);
+			})
+			.catch(function(err) {
+				console.log("There was a failure calling addContactInList");
+				console.error(err);
+				reject(err);
+			});
+    } catch (err) {
+      console.log(err);
+      reject(err);
+    }
+  });
+}
+
+/**
+ *
+ * @param {string} listname Name of the contact list
+ *
+ * @returns {object} contact list information
+ */
+function getContactListByName(listname) {
+  return new Promise(function(resolve, reject) {
+    try {
+		var apiInstance = new platformClient.OutboundApi();
+		
+		var opts = { 
 			'includeImportStatus': false, // Boolean | Include import status
 			'includeSize': false, // Boolean | Include size
 			'pageSize': 25, // Number | Page size. The max that will be returned is 100.
@@ -190,7 +319,7 @@ function getContactListByName(listname) {
 			});
     } catch (err) {
       console.log(err);
-      reject();
+      reject(err);
     }
   });
 }
@@ -203,25 +332,33 @@ function getContactListByName(listname) {
  * @param {string} lastName Last name of any item
  * @param {string} telephone Telephone number of any item
  *
- * @returns {boolean} true
+ * @returns {boolean} success information
  */
 function fillMultipleContactListById(listid,nbOccurence,firstName,lastName,telephone) {
-	var nbContact = 0;
-	if (nbOccurence>99) nbOccurence = 99; // On restreint à 99 elements
-	for (let i = 1; i <= nbOccurence; i++) {
-		var uid = '00'+i;
-		if (uid>=10) {
-			uid = '0'+i;
+	return new Promise(function(resolve, reject) {
+		try {
+			var nbContact = 0;
+			if (nbOccurence>99) nbOccurence = 99; // On restreint à 99 elements
+			for (let i = 1; i <= nbOccurence; i++) {
+				var uid = '00'+i;
+				if (uid>=10) {
+					uid = '0'+i;
+				}
+				
+				console.log('uid : '+uid);
+				addContactInList(uid,listid,firstName,lastName,telephone).then((listitem) => {
+					nbContact++;
+				}).catch((error) => {
+					console.error(error);
+					reject(error);
+				});
+			}
+			resolve(true);
+		} catch (err) {
+		  console.log(err);
+		  reject(err);
 		}
-		
-		console.log('uid : '+uid);
-		addContactInList(uid,listid,firstName,lastName,telephone).then((listitem) => {
-			nbContact++;
-		}).catch((error) => {
-			console.error(error);
-		});
-	}
-	return true;
+	});
 }
 
 /**
